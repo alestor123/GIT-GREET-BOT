00147308ca2e9b04921b8555e3120b6519a4e53b
alestor123 <alestoraldous@gmail.com>
2021-01-15 21:51:53 +0530

    modules

---
+++ App.js
@@ -0,0 +1,6 @@
+var axios = require('axios'),
+os = require('os'),
+chalk = require('chalk'),
+username = os.userInfo().username,
+month = ['January','February','March','April','May','June','July','August','September','October','November','December'],
+infaces = require('os').networkInterfaces();
