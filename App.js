43f458c428ffb2811b726be438cc5d45f80b5e02
alestor123 <alestoraldous@gmail.com>
2021-01-15 22:05:27 +0530

    added main code

--- App.js
+++ App.js
@@ -3,4 +3,8 @@ os = require('os'),
 chalk = require('chalk'),
 username = os.userInfo().username,
 month = ['January','February','March','April','May','June','July','August','September','October','November','December'],
-infaces = require('os').networkInterfaces();
+inf = require('os').networkInterfaces();
+function getIp(){
+for(da in inf) inf[da].filter((inter) => inter.family === 'IPv4' && inter.internal === false ? inad = inter.address : undefined)
+return inad
+}
